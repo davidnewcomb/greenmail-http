4504cd45b927139bf6b1912ef05fae1e6879b881
linuxhelf <linuxhelf@users.noreply.github.com>
2023-09-25 16:18:41 +0200

    Allow SMTP Port to be configured different from 3025 (#45)
    Thank you for the comprehensive test and table :) Excellent work.

--- src/main/java/uk/co/bigsoft/greenmail/http/commands/SmtpSendEmailCommand.java
+++ src/main/java/uk/co/bigsoft/greenmail/http/commands/SmtpSendEmailCommand.java
@@ -7,9 +7,6 @@ import io.javalin.http.Context;
 import uk.co.bigsoft.greenmail.http.dto.EmailDto;
 
 public class SmtpSendEmailCommand extends BaseHandler{
-
-    private static final String HOST = "localhost";
-    private static final int PORT = 3025;
     
     public SmtpSendEmailCommand(GreenMail greenMail){
         super(greenMail);
@@ -18,7 +15,7 @@ public class SmtpSendEmailCommand extends BaseHandler{
     @Override
     public void handle(Context ctx) throws Exception {
         EmailDto emailDto = ctx.bodyAsClass(EmailDto.class);
-        ServerSetup setup = new ServerSetup(PORT, HOST, ServerSetup.PROTOCOL_SMTP);
+        ServerSetup setup = new ServerSetup(gm.getSmtp().getPort(), gm.getSmtp().getBindTo(), ServerSetup.PROTOCOL_SMTP);
         setup.setServerStartupTimeout(ServerSetup.SERVER_STARTUP_TIMEOUT);
         GreenMailUtil.sendTextEmail(emailDto.getTo(),emailDto.getFrom(),emailDto.getSubject(),emailDto.getMsg(),setup);
         ctx.json("OK");
